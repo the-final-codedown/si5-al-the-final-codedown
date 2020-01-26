85d4d18347102d68a6bdcce4bdb704635e7c7e74
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-01-26 16:04:38 +0100

    Updated run script and docker-compose

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+[[ ! $1 ]] && echo Please provide the argument "micro" or "hybrid" && exit
+ARCH=$1
+
 docker-compose run --rm start_dependencies
-docker-compose up -d app cap transfer
+docker-compose -f docker-compose.yml -f docker-compose."$ARCH".yml up
