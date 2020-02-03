1b87d866e93d59a0453fd5cf4f4f857bb7a420ac
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-02-03 10:41:49 +0100

    Fix benchmark file paths

--- tfc-benchmark/prepare.sh
+++ tfc-benchmark/prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ../tfc-cap-updater/proto/tfc/cap/updater/tfc-cap-updater.proto src/main/protobuf/tfc-cap-updater.proto
-cp ../tfc-transfer-validator/proto/tfc-transfer-validator.proto src/main/protobuf/tfc-transfer-validator.proto
+cp ../tfc-cap-updater/src/proto/tfc-cap-updater.proto src/main/protobuf/tfc-cap-updater.proto
+cp ../tfc-transfer-validator/src/proto/tfc-transfer-validator.proto src/main/protobuf/tfc-transfer-validator.proto
 sbt compile
